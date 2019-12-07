db90f68e8498b2edf7892d82a3c5f4119f6fdcca
fuziki <hymne.shannon.projects@gmail.com>
2019-12-07 19:15:58 +0900

    rename nado

---
+++ HapticsBox/HapticsBox/View/HeartBeats/HeartBeatsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HeartBeatsViewController.swift
+//  HapticsBox
+//
+//  Created by fuziki on 2019/12/07.
+//  Copyright © 2019 fuziki.factory. All rights reserved.
+//
+
+
+import UIKit
+import CoreHaptics
+
+class HeartBeatsViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
