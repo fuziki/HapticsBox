3f9675c43df94d9daed89159fad328c710a626c0
fuziki <hymne.shannon.projects@gmail.com>
2019-12-11 23:39:52 +0900

    update

--- HapticsBox/HapticsBox/Applications/Collision/CollisionViewController.swift
+++ HapticsBox/HapticsBox/Applications/Collision/CollisionViewController.swift
@@ -17,10 +17,7 @@ class CollisionViewController: UIViewController {
     @IBOutlet weak var ballView: UIView!
         
     private var ballSize: Float!
-    private var ballPosition: SIMD2<Float> {
-        let x = Float(ballView.frame.origin.x), y = Float(ballView.frame.origin.y)
-        return SIMD2<Float>(x + ballSize / 2, y + ballSize / 2)
-    }
+    private var ballPosition: SIMD2<Float> = SIMD2<Float>(0, 0)
     private var ballVelocity: SIMD2<Float> = SIMD2<Float>(0, 0)
     private var motionMgr: CMMotionManager!
     private let motionQueue = OperationQueue()
@@ -30,6 +27,9 @@ class CollisionViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         ballSize = Float(ballView.frame.width)
+        let x = Float(ballView.frame.origin.x), y = Float(ballView.frame.origin.y)
+        ballPosition = SIMD2<Float>(x + ballSize / 2, y + ballSize / 2)
+
         
         motionMgr = CMMotionManager()
         motionMgr.startDeviceMotionUpdates(to: motionQueue,
@@ -50,9 +50,42 @@ class CollisionViewController: UIViewController {
     
     private func update(motion: CMDeviceMotion) {
         let g: SIMD2<Float> = SIMD2<Float>(Float(motion.gravity.x), Float(-motion.gravity.y))
-        ballVelocity += g * 0.1
+        calcBallPosition(gravity: g)
+        calcCollision()
+        setBall(position: ballPosition)
+    }
+    
+    private func calcBallPosition(gravity: SIMD2<Float>) {
+        ballVelocity += gravity * 0.35
+        func dampVelocity(v: Float, f: Float) -> Float {
+            if abs(v) < f { return 0 }
+            return (abs(v) - f) * (v > 0 ? 1 : -1)
+        }
+        let sf: Float = 0.03
+        ballVelocity = SIMD2<Float>(dampVelocity(v: ballVelocity.x, f: sf), dampVelocity(v: ballVelocity.y, f: sf))
         let p = ballPosition + ballVelocity
-        setBall(position: p)
+        print("g: \(gravity), v: \(ballVelocity)")
+        ballPosition = p
+    }
+    
+    private func calcCollision() {
+        var x = ballPosition.x
+        if x < ballSize / 2 {
+            x = ballSize / 2
+            ballVelocity.x = 0
+        } else if x > Float(self.view.frame.width) - ballSize / 2 {
+            x = Float(self.view.frame.width) - ballSize / 2
+            ballVelocity.x = 0
+        }
+        var y = ballPosition.y
+        if y < ballSize / 2 {
+            y = ballSize / 2
+            ballVelocity.y = 0
+        } else if y > Float(self.view.frame.height) - ballSize / 2 {
+            y = Float(self.view.frame.height) - ballSize / 2
+            ballVelocity.y = 0
+        }
+        ballPosition = SIMD2<Float>(x, y)
     }
     
     private func setBall(position: SIMD2<Float>) {
