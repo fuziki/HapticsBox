a5bc0b0de4cedebf085b91effc8dbbbf5c2a2030
fuziki <hymne.shannon.projects@gmail.com>
2019-12-16 01:44:01 +0900

    update

--- Server/webclient/tanuConsole.js
+++ Server/webclient/tanuConsole.js
@@ -1,16 +1,11 @@
 
 class TanuConsole {
     static shared = new TanuConsole()
-    create(urlId, statusLabel, messageBoxId) {
+    create(urlId, statusLabel) {
         if (this.ts != null && !this.ts.isClosed()) return
         this.ts = new Tanusan()
         this.ts.wsLog(function(e) {
             document.getElementById(statusLabel).innerHTML = e
-        })
-        this.ts.onMsg(function(msg) {
-            var box = document.getElementById(messageBoxId)
-            box.innerHTML += 'msg: ' + msg + '<br>'
-            box.scrollTop = box.scrollHeight;
         })
         var value = document.getElementById(urlId).value
         this.ts.connectWs(value)
